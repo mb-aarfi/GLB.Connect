00f19c31865d2ae33969f5396d41df11bc86d892
Muhammad Baqir <143268082+mb-aarfi@users.noreply.github.com>
2024-11-07 23:07:45 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {  // Adjust scroll distance as needed
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
